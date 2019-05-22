e83027cb2192322be7822df3c8b198bf0e0f0804
Bayuaditya24 <bayuaditya115@gmail.com>
2019-05-22 15:03:14 +0700

    next

--- app/src/main/java/com/example/recyclewithdetails/Main33Activity.java
+++ app/src/main/java/com/example/recyclewithdetails/Main33Activity.java
@@ -1,7 +1,10 @@
 package com.example.recyclewithdetails;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class Main33Activity extends AppCompatActivity {
 
@@ -9,5 +12,29 @@ public class Main33Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main33);
+        TextView a = (TextView) findViewById(R.id.nama1);
+        Bundle bundle=getIntent().getExtras();
+        String b=bundle.getString("nama");
+        TextView c = (TextView) findViewById(R.id.telpon1);
+        String d=bundle.getString("telpon");
+        TextView e = (TextView) findViewById(R.id.orang1);
+        String f=bundle.getString("orang");
+        TextView g = (TextView) findViewById(R.id.jam1);
+        String h=bundle.getString("jam");
+        TextView i = (TextView) findViewById(R.id.tanggal1);
+        String j=bundle.getString("tanggal");
+        TextView  k = (TextView) findViewById(R.id.pesanan1);
+        String  l=bundle.getString("pesanan");
+        a.setText(b);
+        c.setText(d);
+        e.setText(f);
+        g.setText(h);
+        i.setText(j);
+        k.setText(l);
+
+    }
+    public void kembali(View view){
+        Intent kembali = new Intent(Main33Activity.this,MainActivity.class);
+        startActivity(kembali);
     }
 }
--- app/src/main/java/com/example/recyclewithdetails/Main3Activity.java
+++ app/src/main/java/com/example/recyclewithdetails/Main3Activity.java
@@ -3,18 +3,55 @@ package com.example.recyclewithdetails;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Button;
 
 public class Main3Activity extends AppCompatActivity {
 
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
+        final EditText nama = (EditText) findViewById(R.id.nama);
+        final EditText telpon = (EditText) findViewById(R.id.telpon);
+        final EditText orang = (EditText) findViewById(R.id.orang);
+        final EditText jam = (EditText) findViewById(R.id.jam);
+        final EditText tanggal = (EditText) findViewById(R.id.tanggal);
+        final EditText pesanan = (EditText) findViewById(R.id.pesanan);
+
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent pesantempat = new Intent(Main3Activity.this,Main33Activity.class);
+                if (nama.getText().toString().length()==0){
+                    nama.setError("Nama Belum Diisi");
+                }else if (telpon.getText().toString().length()<11){
+                    telpon.setError("No Telepon Salah");
+                }else if (telpon.getText().toString().length()>12){
+                    telpon.setError("No Telepon Salah");
+                }else if (orang.getText().toString().length()==0){
+                    orang.setError("Jumlah Orang Belum Diisi");
+                }else if (jam.getText().toString().length()==0){
+                    jam.setError("Jam Kedatangan Belum Diisi");
+                }else if (tanggal.getText().toString().length()==0){
+                    tanggal.setError("Tanggal Belum Diisi");
+                }else if (pesanan.getText().toString().length()==0){
+                    pesanan.setError("Pesanan Belum Diisi");
+                }else {
+                    pesantempat.putExtra("nama", nama.getText().toString());
+                    pesantempat.putExtra("telpon", telpon.getText().toString());
+                    pesantempat.putExtra("orang", orang.getText().toString());
+                    pesantempat.putExtra("jam", jam.getText().toString());
+                    pesantempat.putExtra("tanggal", tanggal.getText().toString());
+                    pesantempat.putExtra("pesanan", pesanan.getText().toString());
+                    startActivity(pesantempat);
+                }
+            }
+
+
+        });
     }
 
-    //public void pesan(View view) {
-      //  Intent lanjutpesan= new Intent(Main3Activity.this,Main33Activity.class);
-      //  startActivity(lanjutpesan);
-    //}
 }
